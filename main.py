1cef8cfad963a9ba20196a829bf19f67c818f9ad
mohamed-ben-ticha <mohamed.benticha@supcom.tn>
2024-01-30 10:39:58 +0100

    nvm

--- main.py
+++ main.py
@@ -5,18 +5,13 @@ from cnnClassifier.pipeline.stage_02_prepare_base_model import (
 )
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
-import os
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage : {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
-    if os.path.exists("artifacts\data_ingestion\data"):
-        logger.info(f">>>>>> Data already exists <<<<<<")
-        pass
-    else:
-        data_ingestion.main()
+    data_ingestion.main()
     logger.info(f">>>>>> stage : {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
